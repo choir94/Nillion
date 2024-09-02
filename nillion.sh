99f1cab28058998856aed71f61f1eb26eae0e0c1
Ahmad Choiruddin <83292508+choir94@users.noreply.github.com>
2024-09-02 18:38:42 +0700

    Update nillion.sh

--- nillion.sh
+++ nillion.sh
@@ -1,84 +1,101 @@
 #!/bin/bash
 
-echo "Welcome to the Nillion Verifier auto-installer by Airdrop Node"
 echo ""
+echo "Join: https://t.me/airdrop_node"
+read -p "Have you join t.me/airdrop_node on tele? (y/Y to proceed): " JOINED
 
-cd $HOME
-mkdir -p nillion && cd nillion
-
-while true; do
-    echo "1. Visit: https://verifier.nillion.com/verifier, Connect your Keplr wallet, then click Verifier"
-    echo "2. Fund your connected wallet with NIL (Request Faucet here https://faucet.testnet.nillion.com/"
-    read -p "Have you already funded your wallet? (y/n): " funded_wallet
-    if [ "$funded_wallet" == "y" ]; then
-        break
-    else
-        echo "Waiting for you to fund your wallet..."
-    fi
-done
-
-echo "Updating system and installing dependencies..."
-sudo apt update && sudo apt upgrade -y
-sudo apt install -y screen jq
+if [[ ! "$FOLLOWED" =~ ^[yY]$ ]]; then
+    echo ""
+    echo "Please join t.me/airdrop_node on tele before proceeding."
+    exit 1
+fi
 
 if ! command -v docker &> /dev/null; then
-    echo "Docker is not installed. Installing Docker version 27.1.1, build 63125853e3..."
-    sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt update
-    sudo apt install -y docker-ce=5:27.1.1~3-0~ubuntu-$(lsb_release -cs) docker-ce-cli=5:27.1.1~3-0~ubuntu-$(lsb_release -cs) containerd.io
+    echo ""
+    echo "Docker not found. Installing Docker..."
+    sudo apt update && sudo apt install -y curl && \
+    curl -fsSL https://get.docker.com -o get-docker.sh && \
+    sudo sh get-docker.sh
+    sudo usermod -aG docker ${USER} && su - ${USER} -c "groups"
 else
-    echo "Docker is already installed, skipping installation..."
+    echo ""
+    echo "Docker is already installed."
 fi
 
-echo "Pulling the accuser image from Docker Hub..."
-docker pull nillion/retailtoken-accuser:v1.0.0
-
-echo "Initializing the accuser..."
-mkdir -p nillion/accuser
-docker run -v $(pwd)/nillion/accuser:/var/tmp nillion/retailtoken-accuser:v1.0.0 initialise
-
-echo "Displaying accuser credentials:"
-credentials_file="nillion/accuser/credentials.json"
-if [ -f "$credentials_file" ]; then
-    address=$(jq -r '.address' $credentials_file)
-    pub_key=$(jq -r '.pub_key' $credentials_file)
-    echo "Address: $address"
-    echo "Public Key: $pub_key"
+if [ -d "nillion" ]; then
+    echo ""
+    echo "'nillion' directory found. Removing..."
+    sudo rm -r "nillion"
 else
-    echo "credentials.json not found!"
+    echo ""
+    echo "'nillion' directory not found."
 fi
+sudo apt update && sudo apt install -y jq bc
+echo ""
+echo "Stopping and removing any running containers with the name 'nillion'..."
+sudo docker ps | grep nillion | awk '{print $1}' | xargs -r docker stop
+sudo docker ps -a | grep nillion | awk '{print $1}' | xargs -r docker rm
 
-while true; do
-    echo "Please copy your account_id and public_key to https://verifier.nillion.com/verifier."
-    read -p "Have you completed this step? (y/n): " copied_details
-    if [ "$copied_details" == "y" ]; then
-        break
+echo ""
+echo "Pulling NILLION docker image..."
+sudo docker pull nillion/retailtoken-accuser:latest
+
+echo ""
+echo "Creating directory and running Docker container to initialize..."
+mkdir -p nillion/accuser && \
+sudo docker run -v "$(pwd)/nillion/accuser:/var/tmp" nillion/retailtoken-accuser:latest initialise
+
+SECRET_FILE=~/nillion/accuser/credentials.json
+if [ -f "$SECRET_FILE" ]; then
+    ADDRESS=$(jq -r '.address' "$SECRET_FILE")
+    echo ""
+    echo "Request nillion faucet (https://faucet.testnet.nillion.com) to your accuser wallet address: $ADDRESS"
+    echo ""
+
+    read -p "Have you requested the faucet to the accuser wallet? (y/Y to proceed): " FAUCET_REQUESTED1
+    if [[ "$FAUCET_REQUESTED1" =~ ^[yY]$ ]]; then
+        echo ""
+        echo "Now visit: https://verifier.nillion.com/verifier"
+        echo "Connect a new Keplr wallet."
+        echo "Request faucet to the nillion address : https://faucet.testnet.nillion.com"
+        echo ""
+
+        read -p "Have you requested faucet to your Keplr wallet? (Y/y to proceed): " FAUCET_REQUESTED2
+        if [[ "$FAUCET_REQUESTED2" =~ ^[yY]$ ]]; then
+            read -p "Input your Keplr wallet's nillion address: " KEPLR
+            echo ""
+            echo "Input the following information on the website: https://verifier.nillion.com/verifier"
+            echo "Address: $ADDRESS"
+            echo "Public Key: $(jq -r '.pub_key' "$SECRET_FILE")"
+            echo ""
+
+            read -p "Have you done this? (Y/y to proceed): " address_submitted
+            if [[ "$address_submitted" =~ ^[yY]$ ]]; then
+                echo ""
+                echo "Save this Private Key in safe place: $(jq -r '.priv_key' "$SECRET_FILE")"
+                echo ""
+                read -p "Have you saved the private key in a safe place? (y/Y to proceed): " private_key_saved
+                if [[ "$private_key_saved" =~ ^[yY]$ ]]; then
+                    echo ""
+                    echo "Running Docker container with accuse command..."
+                    sudo docker run -v "$(pwd)/nillion/accuser:/var/tmp" nillion/retailtoken-accuser:latest accuse --rpc-endpoint "https://testnet-nillion-rpc.lavenderfive.com/" --block-start "$(curl -s "https://testnet-nillion-api.lavenderfive.com/cosmos/tx/v1beta1/txs?query=message.sender='$KEPLR'&pagination.limit=20&pagination.offset=0" | jq -r '[.tx_responses[] | select(.tx.body.memo == "AccusationRegistrationMessage")] | sort_by(.height | tonumber) | .[-1].height | tonumber - 5' | bc)"
+                else
+                    echo ""
+                    echo "Please save the private key and try again."
+                fi
+            else
+                echo ""
+                echo "Please complete the submission and try again."
+            fi
+        else
+            echo ""
+            echo "Please request the faucet to your Keplr wallet and try again."
+        fi
     else
-        echo "Waiting for you to copy your account_id and public_key..."
+        echo ""
+        echo "Please request the faucet and try again."
     fi
-done
-
-while true; do
-    echo "Please fund your accuser address (account_id) on https://faucet.testnet.nillion.com/."
-    read -p "Have you funded your accuser address? (y/n): " funded_accuser
-    if [ "$funded_accuser" == "y" ]; then
-        break
-    else
-        echo "Waiting for you to fund your accuser address..."
-    fi
-done
-
-echo "Waiting 30-60 minutes before running the accuser..."
-sleep_time=$((30 + RANDOM % 31))m
-echo "Sleeping for $sleep_time..."
-sleep $sleep_time
-
-current_height=$(curl -s https://testnet-nillion-rpc.lavenderfive.com/abci_info | jq -r '.result.response.last_block_height')
-block_start=$((current_height - 3))
-
-echo "Running the accuser..."
-docker run -v $(pwd)/nillion/accuser:/var/tmp nillion/retailtoken-accuser:v1.0.0 accuse --rpc-endpoint "https://testnet-nillion-rpc.lavenderfive.com/" --block-start $block_start
-
-echo "The accuser is now running and will automatically accuse once the registration event is posted to the chain."
+else
+    echo ""
+    echo "credentials.json file not found. Ensure the initialization step completed successfully."
+fi
